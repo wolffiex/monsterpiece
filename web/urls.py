be20e1147d1595bddf1e7798599beccaa0652340
Adam Wolff <adam@reginald.tail5d4e4.ts.net>
2024-04-04 18:42:25 -0700

    Float not working

--- web/urls.py
+++ web/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from web.views import LoginView, TopicView
+from web.views import LoginView, TopicView, ThreadsView
 from django.shortcuts import redirect
 
 def root_redirect(request):
@@ -12,5 +12,5 @@ urlpatterns = [
     path("", root_redirect, name="index"),
     path("login", LoginView.as_view(), name="login"),
     path("topics", TopicView.as_view(), name="topics"),
+    path("topics/<int:id>", ThreadsView.as_view(), name="threads"),
 ]
-
--- web/views.py
+++ web/views.py
@@ -5,28 +5,33 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from dataclasses import dataclass
+from datetime import datetime
+
 
 class LoginView(View):
     def get(self, request):
         if request.user.is_authenticated:
-            return redirect('topics')
+            return redirect("topics")
         return render(request, "login.html", {})
 
     def post(self, request):
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+        username = request.POST.get("username")
+        password = request.POST.get("password")
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('topics')  # Redirect to the app view
+            return redirect("topics")  # Redirect to the app view
         else:
-            messages.error(request, 'Invalid username or password.')
-            return render(request, 'login.html', {})
+            messages.error(request, "Invalid username or password.")
+            return render(request, "login.html", {})
+
+
 @dataclass
 class Topic:
-    emoji_string : str
+    emoji_string: str
     label: str
     convo_count: int
+
 
 class TopicView(View):
     @method_decorator(login_required)
@@ -35,4 +40,25 @@ class TopicView(View):
         topics.append(Topic("&#x1F9D1;&#x200D;&#x1F373;", "Cooking", 1))
         topics.append(Topic("&#x1F6F0;&#xFE0F", "Raspberry Pi", 4))
         topics.append(Topic("&#x1F426;", "Tweets", 6))
-        return render(request, 'topics.html', {"topics": topics})
+        return render(request, "topics.html", {"topics": topics})
+
+
+@dataclass
+class Thread:
+    summary: str
+    last_updated: datetime
+    tokens: int
+
+
+class ThreadsView(View):
+    @method_decorator(login_required)
+    def get(self, request, id):
+        print(id)
+        threads = []
+        threads.append(
+            Thread("django template padding unicode characters", datetime.now(), 1000)
+        )
+        threads.append(
+            Thread("postgres default transaction isolation", datetime.now(), 2000)
+        )
+        return render(request, "threads.html", {"threads": threads})
